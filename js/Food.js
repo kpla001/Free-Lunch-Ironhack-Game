5cdbe10bf2ec1e434244eb25c8985d7e086c30b3
kpla001 <kpla001@fiu.edu>
2021-07-29 22:13:15 -0400

    fix load game

--- js/Food.js
+++ js/Food.js
@@ -7,14 +7,14 @@ var foodImage6 = new Image();
 var foodImage7 = new Image();
 var foodImage8 = new Image();
 
-foodImage1.src = "./images/chicken.png";
-foodImage2.src = "./images/fries.png";
-foodImage3.src = "./images/hamburger.png";
-foodImage4.src = "./images/hotdog.png";
-foodImage5.src = "./images/jerky.png";
-foodImage6.src = "./images/pizza.png";
-foodImage7.src = "./images/sushi.png";
-foodImage8.src = "./images/taco.png";
+foodImage1.src = "./Images/chicken.png";
+foodImage2.src = "./Images/fries.png";
+foodImage3.src = "./Images/hamburger.png";
+foodImage4.src = "./Images/hotdog.png";
+foodImage5.src = "./Images/jerky.png";
+foodImage6.src = "./Images/pizza.png";
+foodImage7.src = "./Images/sushi.png";
+foodImage8.src = "./Images/taco.png";
 ////////////////////////////////////////////////////////////////
 
 class Food extends Component {
--- js/Trash.js
+++ js/Trash.js
@@ -7,18 +7,42 @@ var trashImage6 = new Image();
 var trashImage7 = new Image();
 var trashImage8 = new Image();
 
-trashImage1.src = "./images/bananapeel.png";
-trashImage2.src = "./images/cardboard.png";
-trashImage3.src = "./images/chair.png";
-trashImage4.src = "./images/papertrash.png";
-trashImage5.src = "./images/tincan.png";
-trashImage6.src = "./images/tire.png";
-trashImage7.src = "./images/trashbag.png";
-trashImage8.src = "./images/wrapper.png";
+trashImage1.src = "./Images/bananapeel.png";
+trashImage2.src = "./Images/cardboard.png";
+trashImage3.src = "./Images/chair.png";
+trashImage4.src = "./Images/papertrash.png";
+trashImage5.src = "./Images/tincan.png";
+trashImage6.src = "./Images/tire.png";
+trashImage7.src = "./Images/trashbag.png";
+trashImage8.src = "./Images/wrapper.png";
 /////////////////////////////////////////////////////////////////////////////
 
 class Trash extends Component {
-    constructor(classGame, x, y, width, height, imageSrc) {
+    constructor(classGame, x, y, width, height, imageSrc, context) {
         super(classGame, x, y, width, height, imageSrc)
+        this.context = context;
+        // this.trashArray = [];
+        this.trash = new Image();
+        this.trash.src = imageSrc;
     }
+
+    drawTrash() {
+            this.context.drawImage(this.trash, this.x, this.y, this.width, this.height)
+        }
+
+    // put in game class
+
+    // addTrash() {
+    //     for(let i = 0; i < trashType.length; i++) {
+    //         const trashImage = new Image();
+    //         trashImage.src = `./images/${trashType[i]}.png`
+    //         this.trashArray.push({[`trashImage${i+1}`]: trashImage});
+    //     }
+    // }
+
+    // drawTrash() {
+    //     this.trashArray.forEach(trash => {
+    //         this.context.drawImage(trash, )
+    //     })
+    // }
 }
