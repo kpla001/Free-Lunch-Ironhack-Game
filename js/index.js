9177027d7fce57aab5a9d10878b2181b0c359c0a
kpla001 <kpla001@fiu.edu>
2021-07-29 22:09:56 -0400

    winodw onload

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
-const startButton = document.getElementById('start-button');
-const restartButton = document.getElementById('restart-button');
-const landingPage = document.querySelector('.landing-page.instructions');
-const canvasHTML = document.getElementById('canvas');
+window.onload = () => {
+  const startButton = document.getElementById('start-button');
+  const restartButton = document.getElementById('restart-button');
+  const landingPage = document.querySelector('.landing-page.instructions');
+  const canvasHTML = document.getElementById('canvas');
 
-let game = new Game();
+  let game = new Game();
 
 
-startButton.addEventListener('click', () => {
-  landingPage.classList.toggle('hidden');
-  startButton.classList.toggle('hidden');
-  restartButton.classList.toggle('hidden');
-  canvasHTML.style.display = "block";
-  startGame();
-});
+  startButton.addEventListener('click', () => {
+    landingPage.classList.toggle('hidden');
+    startButton.classList.toggle('hidden');
+    restartButton.classList.toggle('hidden');
+    canvasHTML.style.display = "block";
+    startGame();
+  });
 
-restartButton.addEventListener('click', () => {
-  game.backgroundAudio.pause();
-  game = new Game();
-  startGame();
-});
+  restartButton.addEventListener('click', () => {
+    game.backgroundAudio.pause();
+    game = new Game();
+    startGame();
+  });
 
-// window.onload = () => {
-//     document.getElementById('start-button').onclick = () => {
-//       startGame();
-//     };
+  // window.onload = () => {
+  //     document.getElementById('start-button').onclick = () => {
+  //       startGame();
+  //     };
 
-function startGame() {
+  function startGame() {
     return  game.start();
-    };
+  };
+}
